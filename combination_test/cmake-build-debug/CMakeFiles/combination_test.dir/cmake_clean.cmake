332b6e73efc69dcf6653662221bd61922b00d9d8
Lucas Barros <43018309+mocreviz@users.noreply.github.com>
2019-11-04 12:08:09 -0300

    algoritmo

---
+++ combination_test/cmake-build-debug/CMakeFiles/combination_test.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/combination_test.dir/main.c.obj"
+  "combination_test.pdb"
+  "combination_test.exe"
+  "combination_test.exe.manifest"
+  "libcombination_test.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/combination_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
